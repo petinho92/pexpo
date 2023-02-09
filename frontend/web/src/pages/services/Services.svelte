83dd4714694c2bf90683e323776a265b75743b6a
MASO09 <tenzlingerkristof@gmail.com>
2023-02-09 19:49:27 +0100

    feat: Service.svelte added

---
+++ frontend/web/src/pages/services/Services.svelte
@@ -0,0 +1,200 @@
+<div class="columns is-centered">
+    <div class="column has-text-centered is-two-thirds">
+        <div class="is-size-2 has-text-weight-bold mt-3 mb-6">Pollack Expo kiállítói információk</div>
+        <div class="has-text-left is-size-5 mt-5">
+            <div class=" has-text-weight-bold">
+                A kiállítás és konferencia időpontja
+            </div>
+            <div>
+                2023. április 13-14.
+            </div>
+        </div>
+        <div class="has-text-left is-size-5 mt-5">
+            <div class=" has-text-weight-bold">
+                Helyszín
+            </div>
+            <div>
+                Pécsi Expo Center, 7631 Pécs, Megyeri út 72.
+            </div>
+        </div>
+        <div class="has-text-left  is-size-5 mt-5">
+            <div class=" has-text-weight-bold">
+                A rendezvény tervezett programja főbb vonalakban
+            </div>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Időpont</th>
+                    <th>Program</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Április 12. 14:00‐19:00</td>
+                    <td>Kiállítás berendezése</td>
+                </tr>
+                <tr>
+                    <td>Április 12. 19:00</td>
+                    <td>Borkóstolóval egybekötött vacsora (opcionális)</td>
+                </tr>
+                <tr>
+                    <td>Április 13. 9:30</td>
+                    <td>Kiállítás és szaknapok megnyitása, plenáris előadás</td>
+                </tr>
+                <tr>
+                    <td>Április 13. 14:00-17:00</td>
+                    <td>Szakmai előadások több szekcióban</td>
+                </tr>
+                <tr>
+                    <td>Április 13. 19:00</td>
+                    <td>Fogadás a kiállítók részére a PTE MIK Boszorkány úti aulájában</td>
+                </tr>
+                <tr>
+                    <td>Április 14. 9:00‐14:00</td>
+                    <td>Szakmai előadások több szekcióban</td>
+                </tr>
+                <tr>
+                    <td>Április 14. 14:00-16:00</td>
+                    <td>Kiállítás bontása</td>
+                </tr>
+
+                </tbody>
+            </table>
+
+        </div>
+        <div class="has-text-left is-size-5 mt-5">
+            <div class="has-text-weight-bold">
+                Stand
+            </div>
+            <div class="has-text-justified">
+                A kiállítói standok SYMA elemekből épülnek egységesen méretben (3×2=6 m2).
+                A standhoz 1 db asztalt, 2 db széket,
+                valamint 1 db, 230 V-os hálózati csatlakozást biztosítunk.
+                Kérjük, hogy laptop, képernyő, egyéb elektromos felhasználású eszköz
+                csatlakoztatásához gondoskodjanak saját elosztóról!
+            </div>
+            <div class="content mt-6">
+                Választható kiállítói alapterületek:
+                <ul class="ml-6">
+                    <li>a kiállításszervező által épített stand (3×2=6 m2)</li>
+                    <li>saját építésű stand (10 m2)</li>
+                </ul>
+            </div>
+            <figure class="image is-fullwidth">
+                <img src="/~web/images/standkep.jpg" alt="stand">
+            </figure>
+            <div class="has-text-centered is-size-6">
+                A képen dupla méretű (6×2 = 12 m2) stand látható.
+            </div>
+            <div class="mt-6">
+                A frízfelirat maximális mérete:
+            </div>
+            <figure class="image is-fullwidth">
+                <!--                TODO: nem tudom word-ből kiszedni a friz felirat méretes képet-->
+                <img src="/~web/images/" alt="Itt lenne a frizes kép">
+            </figure>
+            <div class="has-text-weight-bold has-text-centered">
+                Egységes frízfeliratokat nem áll módunkban biztosítani!
+            </div>
+
+        </div>
+        <div class="has-text-left is-size-5 mt-6">
+            <table class="table">
+                <tbody>
+                <tr>
+                    <td>A szervező által biztosított stand (6 m <sup>2</sup>)</td>
+                    <td>250.000, Ft + ÁFA </td>
+                </tr>
+                <tr>
+                    <td>Saját építésű stand (10 m <sup>2</sup>)</td>
+                    <td>250.000, Ft + ÁFA</td>
+                </tr>
+                <tr>
+                    <td>Plusz terület (Ft/ m <sup>2</sup>)</td>
+                    <td>42.000, - Ft + ÁFA</td>
+                </tr>
+                <tr>
+                    <td>3 fázis kiépítése</td>
+                    <td>25.000,- Ft + ÁF</td>
+                </tr>
+                <tr>
+                    <td>Állásbörze (csoportos standlátogatás)</td>
+                    <td>20.000,- Ft + ÁFA</td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+        <div class="has-text-left  is-size-5 mt-5">
+            <div class=" has-text-weight-bold">
+                Catering
+            </div>
+            <table class="table">
+                <tbody>
+                <tr>
+                    <td>Borkostoló és vacsora – 2023.04.12.</td>
+                    <td>12.500, Ft + ÁFA/fő</td>
+                </tr>
+                <tr>
+                    <td>Ebédjegy a csütörtöki napra – 2023.04.13.</td>
+                    <td>2.500, Ft + ÁFA/fő</td>
+                </tr>
+                <tr>
+                    <td>Ebédjegy a pénteki napra – 2023.04.14.</td>
+                    <td>2.500, Ft + ÁFA/fő</td>
+                </tr>
+                <tr>
+                    <td>Belépőjegy a csütörtök esti fogadásra</td>
+                    <td>15.000, Ft + ÁFA/fő</td>
+                </tr>
+
+                </tbody>
+            </table>
+            <div class="mt-3">
+                <div class="is-underlined">
+                    A borkóstolós vacsora helyszíne:
+                </div>
+                <div>
+                    Tettye Vendéglő, 7625 Pécs, Tettye tér 5.
+                </div>
+            </div>
+            <div class="mt-3">
+                <div class="is-underlined">
+                    A csütörtök esti fogadás helyszíne:
+                </div>
+                <div class="">
+                    Pécsi Tudományegyetem, Műszaki és Informatikai Kar Aula
+                </div>
+                <div>
+                    7624 Pécs, Boszorkány út 2.
+                </div>
+            </div>
+            <div class="mt-3">
+                <div class="is-underlined">
+                    Az ebédeket a kiállítók a rendezvény helyszínén vehetik igénybe:
+                </div>
+                <div>
+                    Pécsi Expo Center, 7631 Pécs, Megyeri út 72.
+                </div>
+            </div>
+
+        </div>
+        <div class="has-text-left is-size-5 mt-5">
+            <div class="has-text-weight-bold">
+                Regisztráció, számlázás
+            </div>
+            <div>
+                A fenti szolgáltatások a Pollack Expo weboldalán található kiállítói regisztrációval vehetők igénybe:
+                <a href="http://www.pollackexpo.hu">http://www.pollackexpo.hu</a>
+            </div>
+            <div>
+                A fizetés díjbekérő alapján történik. A díjbekérő kiegyenlítése után a
+                számlát a regisztrációban megadott kapcsolattartó e-mail címére megküldjük
+            </div>
+        </div>
+
+
+
+    </div>
+
+</div>
