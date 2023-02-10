8b43f6fa27864bc1b0a988d3010b75965af21aa1
MASO09 <tenzlingerkristof@gmail.com>
2023-02-10 14:35:28 +0100

    feat: programguide

---
+++ frontend/web/src/pages/programguide/ProgramGuide.svelte
@@ -0,0 +1,76 @@
+<script>
+    import {programguide} from "./data/pguide.ts";
+    import Footer from 'src/components/Footer.svelte';
+    import {_} from 'svelte-i18n';
+    import {locale} from 'svelte-i18n';
+
+</script>
+
+
+<section class="section">
+    <div class="container">
+        {#each programguide as pg}
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src={pg.thumb} alt={pg.alt}>
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="content">
+                        <h4>{pg.head}</h4>
+                        <p class="ribbon">{pg.date}</p>
+                        {#if $locale === 'hu'}
+                            <p>{pg.type}</p>
+                        {/if}
+                        {#if $locale === 'en'}
+                            <p>{pg.type_en}</p>
+                        {/if}
+
+                        <a href={pg.path} class="card-button">
+                            {$_('programguide.button')}
+                        </a>
+                        <br>
+                        {#if (pg.path_en !== "")}
+                            <a href={pg.path_en} class="card-button">
+                                {$_('programguide.button_en')}
+                            </a>
+                        {/if}
+                    </div>
+
+
+                </div>
+            </div>
+        {/each}
+    </div>
+
+</section>
+
+<style>
+    .container {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        grid-gap: 0.5em;
+    }
+
+    .card {
+        height: max-content;
+    }
+
+    .card-button {
+        display: flex;
+        justify-content: center;
+        padding: 10px 0;
+        width: 100%;
+        background-color: #1F487E;
+        color: #fff;
+        border-radius: 8px;
+    }
+
+    .card-button:hover {
+        text-decoration: none;
+        background-color: #1D3461;
+        color: #fff;
+
+    }
+</style>
---
+++ frontend/web/src/pages/programguide/data/pguide.ts
@@ -0,0 +1,91 @@
+export let programguide = [
+    {
+        id: 1,
+        path: "/~web/assets/programguide/ProgramGuide_2022_web.pdf",
+        path_en: "/~web/assets/programguide/ProgramGuide_2022_web_en.pdf",
+        thumb: '/~web/images/pg_tumb/pg_2022.png',
+        alt: "PollackExpo 2022 ProgramGuide",
+        head: 'PollackExpo 2022',
+        date: '2022. február 24-26.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    },
+    {
+        id: 2,
+        path: "/~web/assets/programguide/ProgramGuide_2020_web.pdf",
+        path_en: "",
+        thumb: '/~web/images/pg_tumb/pg_2020.PNG',
+        alt: "PollackExpo 2020 ProgramGuide",
+        head: 'PollackExpo 2020',
+        date: '2020. február 27-28.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    },
+    {
+        id: 3,
+        path: '/~web/assets/programguide/ProgramGuide_2019_web.pdf',
+        path_en: "",
+        thumb: '/~web/images/pg_tumb/pg_2019.PNG',
+        alt: 'PollackExpo 2019 ProgramGuide',
+        head: 'PollackExpo 2019',
+        date: '2019. február 28.- március 1.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    },
+    {
+        id: 4,
+        path: '/~web/assets/programguide/ProgramGuide2018_web.pdf',
+        path_en: "",
+        thumb: '/~web/images/pg_tumb/pg_2018.PNG',
+        alt: 'PollackExpo 2018 ProgramGuide',
+        head: 'PollackExpo 2018',
+        date: '2018. március 1-2.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    },
+    {
+        id: 5,
+        path: '/~web/assets/programguide/ProgramGuide2017_web.pdf',
+        path_en: "",
+        thumb: '/~web/images/pg_tumb/pg_2017.PNG',
+        alt: 'PollackExpo 2017 ProgramGuide',
+        head: 'PollackExpo 2017',
+        date: '2017. február 23-24.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    },
+    {
+        id: 6,
+        path: '/~web/assets/programguide/Programfuzet_PollackExpo2016_web.pdf',
+        path_en: "",
+        thumb: '/~web/images/pg_tumb/pg_2016.PNG',
+        alt: 'PollackExpo 2016 ProgramGuide',
+        head: 'PollackExpo 2016',
+        date: '2016. február 25-26.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    },
+    {
+        id: 7,
+        path: '/~web/assets/programguide/Programfuzet_PollackExpo2015_web.pdf',
+        path_en: "",
+        thumb: '/~web/images/pg_tumb/pg_2015.PNG',
+        alt: 'PollackExpo 2015 ProgramGuide',
+        head: 'PollackExpo 2015',
+        date: '2015. február 26-27.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    },
+    {
+        id: 8,
+        path: '/~web/assets/programguide/Programfuzet_PollackExpo2014.pdf',
+        path_en: "",
+        thumb: '/~web/images/pg_tumb/pg_2014.PNG',
+        alt: 'PollackExpo 2014 ProgramGuide',
+        head: 'PollackExpo 2014',
+        date: '2014. február 27-28.',
+        type: 'Szakmai Kiállítás és konferencia Programfüzet',
+        type_en: 'Trade Exhibition and Conference Programme'
+    }
+
+];
