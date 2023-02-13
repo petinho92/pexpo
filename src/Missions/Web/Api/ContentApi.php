6f55e30f970eceb2bf5f7913543939ede32091e3
petinho <petinho92@gmail.com>
2023-02-13 17:24:14 +0100

    remove getall method, created new endpoint

--- src/Missions/Web/Api/ContentApi.php
+++ src/Missions/Web/Api/ContentApi.php
@@ -7,15 +7,40 @@ use Atomino\Mercury\Responder\Api\Attributes\Route;
 
 class ContentApi extends Api{
 
-    #[Route(self::GET,  '/getcontent/getall')]
-    public function getAll(){
-        return Content::search()->collect();
-    }
-
     #[Route(self::GET,  '/get/:page')]
     public function getpage(string $page){
-        return Content::search(Filter::where(Content::page($page)))->collect();
+        $tmp = Content::search(Filter::where(Content::page($page)))->collect();
+        $array = array();
+        foreach ($tmp as $content){
+            $array[] = (object)[
+              'page' => $content->page,
+              'content_hu' => $content->content_hu,
+              'content_en' => $content->content_en
+            ];
+        }
+        return $array;
     }
 
+    #[Route(self::GET, '/get/:page/:lang')]
+    public function getContentByLang(string $page, string $lang){
+        $tmp = Content::search(Filter::where(Content::page($page)))->collect();
+
+        foreach ($tmp as $content){
+            if($lang == "hu") return $content->content_hu;
+            else if($lang == "en") return $content->content_en;
+            else return null;
+        }
+    }
+
+    #[Route(self::GET, '/get/:page/:category/:lang')]
+    public function getContentByLangContentPage(string $page, string $category, string $lang){
+        $tmp = Content::search(Filter::where(Content::page($page))->and(Content::category($category)));
+
+        foreach ($tmp as $content){
+            if($lang == "hu") return $content->content_hu;
+            else if($lang == "en") return $content->content_en;
+            else return null;
+        }
+}
 
 }
