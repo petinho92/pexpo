df14c8ee96c5e159767ea58be048bb76facc9bb6
petinho <petinho92@gmail.com>
2023-02-13 14:23:23 +0100

    updated endpoint

--- src/Missions/Web/Api/ContentApi.php
+++ src/Missions/Web/Api/ContentApi.php
@@ -1,6 +1,7 @@
 <?php namespace Application\Missions\Web\Api;
 
 use Application\Entity\Content;
+use Atomino\Carbon\Database\Finder\Filter;
 use Atomino\Mercury\Responder\Api\Api;
 use Atomino\Mercury\Responder\Api\Attributes\Route;
 
@@ -9,6 +10,11 @@ class ContentApi extends Api{
     #[Route(self::GET,  '/getcontent/getall')]
     public function getAll(){
         return Content::search()->collect();
+    }
+
+    #[Route(self::GET,  '/get/:page')]
+    public function getpage(string $page){
+        return Content::search(Filter::where(Content::page($page)))->collect();
     }
 
 
