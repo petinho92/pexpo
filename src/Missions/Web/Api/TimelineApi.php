fa00961440bfb2e266c1c7c96ec26c70ac3606f6
petinho <petinho92@gmail.com>
2023-02-14 15:08:50 +0100

    added timeline api

---
+++ src/Missions/Web/Api/TimelineApi.php
@@ -0,0 +1,29 @@
+<?php namespace Application\Missions\Web\Api;
+
+use Application\Entity\Timeline;
+use Atomino\Carbon\Database\Finder\Filter;
+use Atomino\Mercury\Responder\Api\Api;
+use Atomino\Mercury\Responder\Api\Attributes\Route;
+
+class TimelineApi extends Api{
+
+    #[Route(self::GET, '/all')]
+    function getTimeline(){
+        $result = Timeline::search(Filter::where(Timeline::active(true)))->desc(Timeline::year)->collect();
+        $array = array();
+        foreach ($result as $data) {
+            $array[] = (object)[
+                'id' => $data->id,
+                'year' => $data->year,
+                'hu_title' => $data->hu_title,
+                'en_title' => $data->en_title,
+                'hu_desc' => $data->hu_desc,
+                'en_desc' => $data->en_desc,
+
+
+            ];
+        }
+        return $array;
+    }
+
+}
