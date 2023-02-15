f64a7e551cf45b840aa51c714a1a47571c5036e0
petinho <petinho92@gmail.com>
2023-02-15 08:51:50 +0100

    added NotBlank validator for programguide entity

--- src/Entity/Programguide.php
+++ src/Entity/Programguide.php
@@ -2,10 +2,12 @@
 
 use Atomino\Carbon\Attributes\Modelify;
 use Application\Atoms\Entity\_Programguide;
+use Atomino\Carbon\Attributes\Validator;
 use Atomino\Carbon\Plugins\Attachment\Attachmentable;
 use Atomino\Carbon\Plugins\Attachment\AttachmentCollection;
 use Atomino\Carbon\Plugins\Created\Created;
 use Atomino\Carbon\Plugins\Updated\Updated;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 #[Created]
@@ -13,6 +15,11 @@ use Atomino\Carbon\Plugins\Updated\Updated;
 #[Attachmentable()]
 #[AttachmentCollection(field: "picture", maxCount: 1, mimetype: "/image\/.*/")]
 #[AttachmentCollection(field: "files", maxCount: 2, mimetype: "/application\/pdf/")]
+#[Validator("name", NotBlank::class)]
+#[Validator("date", NotBlank::class)]
+#[Validator("hu_type", NotBlank::class)]
+#[Validator("en_type", NotBlank::class)]
+
 #[Modelify(\Application\Database\DefaultConnection::class, 'programguide', true)]
 class Programguide extends _Programguide{
 
