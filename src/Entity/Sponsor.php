7d228db0e6da66c98827f54ed12bcc2e93b4aa1a
petinho <petinho92@gmail.com>
2023-02-15 08:53:19 +0100

    added NotBlank validator for sponsor entity

--- src/Entity/Sponsor.php
+++ src/Entity/Sponsor.php
@@ -2,16 +2,20 @@
 
 use Atomino\Carbon\Attributes\Modelify;
 use Application\Atoms\Entity\_Sponsor;
+use Atomino\Carbon\Attributes\Validator;
 use Atomino\Carbon\Plugins\Attachment\Attachmentable;
 use Atomino\Carbon\Plugins\Attachment\AttachmentCollection;
 use Atomino\Carbon\Plugins\Created\Created;
 use Atomino\Carbon\Plugins\Updated\Updated;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 #[Created]
 #[Updated]
 #[Attachmentable()]
 #[AttachmentCollection(field: 'picture', maxCount: 1, mimetype: "/image\/.*/")]
+#[Validator("name", NotBlank::class)]
+#[Validator("website", NotBlank::class)]
 #[Modelify(\Application\Database\DefaultConnection::class, 'sponsor', true)]
 class Sponsor extends _Sponsor
 {
