9c572eb3fdb8d1457f2dfcf31f64e55aae969542
petinho <petinho92@gmail.com>
2023-02-15 08:26:28 +0100

    added getTimelineByYear and getTimelineByLang endpoint

--- src/Missions/Web/Api/TimelineApi.php
+++ src/Missions/Web/Api/TimelineApi.php
@@ -7,7 +7,7 @@ use Atomino\Mercury\Responder\Api\Attributes\Route;
 
 class TimelineApi extends Api{
 
-    #[Route(self::GET, '/all')]
+    #[Route(self::GET, '/get/all')]
     function getTimeline(){
         $result = Timeline::search(Filter::where(Timeline::active(true)))->desc(Timeline::year)->collect();
         $array = array();
@@ -25,5 +25,34 @@ class TimelineApi extends Api{
         }
         return $array;
     }
+
+    #[Route(self::GET, '/get/:year')]
+    function getTimelineByYear(int $year){
+        $result = Timeline::search(Filter::where(Timeline::active(true))->and(Timeline::year($year)));
+        $array = array();
+        foreach ($result as $data) {
+            $array[] = (object)[
+                'year' => $data->year,
+                'hu_title' => $data->hu_title,
+                'en_title' => $data->en_title,
+                'hu_desc' => $data->hu_desc,
+                'en_desc' => $data->en_desc
+            ];
+        }
+        return $array;
+    }
+
+    #[Route(self::GET, '/get/:lang')]
+    function getTimelineByLang(string $lang){
+        $result = Timeline::search(Filter::where(Timeline::active(true))->collect());
+
+        foreach ($result as $item){
+            if($lang == 'hu') return ['year' => $item->year, 'hu_title' => $item->hu_title, 'hu_desc' => $item->hu_desc];
+            else if($lang == 'en') return ['year' => $item->year, 'en_title' => $item->en_title, 'en_desc' => $item->en_desc];
+            else return null;
+        }
+
+    }
+
 
 }
