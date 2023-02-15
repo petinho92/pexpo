aed8ef2c358f2e942eb6198ec9297599c0b373b9
petinho <petinho92@gmail.com>
2023-02-15 08:49:53 +0100

    added NotBlank validator for entity

--- src/Entity/Gallery.php
+++ src/Entity/Gallery.php
@@ -2,15 +2,21 @@
 
 use Atomino\Carbon\Attributes\Modelify;
 use Application\Atoms\Entity\_Gallery;
+use Atomino\Carbon\Attributes\Validator;
 use Atomino\Carbon\Plugins\Attachment\Attachmentable;
 use Atomino\Carbon\Plugins\Attachment\AttachmentCollection;
 use Atomino\Carbon\Plugins\Created\Created;
 use Atomino\Carbon\Plugins\Updated\Updated;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[Created]
 #[Updated]
 #[Attachmentable()]
 #[AttachmentCollection(field: 'picture', mimetype: "/image\/.*/")]
+#[Validator('name', NotBlank::class)]
+#[Validator('category', NotBlank::class)]
+#[Validator('alt', NotBlank::class)]
+#[Validator('year', NotBlank::class)]
 #[Modelify(\Application\Database\DefaultConnection::class, 'gallery', true)]
 class Gallery extends _Gallery{
 
