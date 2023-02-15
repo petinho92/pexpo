aa0bdc17c09f5920ac83f8ce2b3ef1e2f41c760a
petinho <petinho92@gmail.com>
2023-02-15 08:54:59 +0100

    added NotBlank validator for timeline entity

--- src/Entity/Timeline.php
+++ src/Entity/Timeline.php
@@ -2,15 +2,22 @@
 
 use Atomino\Carbon\Attributes\Modelify;
 use Application\Atoms\Entity\_Timeline;
+use Atomino\Carbon\Attributes\Validator;
 use Atomino\Carbon\Plugins\Attachment\Attachmentable;
 use Atomino\Carbon\Plugins\Attachment\AttachmentCollection;
 use Atomino\Carbon\Plugins\Created\Created;
 use Atomino\Carbon\Plugins\Updated\Updated;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[Created]
 #[Updated]
 #[Attachmentable()]
 #[AttachmentCollection(field: "picture", maxCount: 1, mimetype: "/image\/.*/")]
+#[Validator("year", NotBlank::class)]
+#[Validator("hu_title", NotBlank::class)]
+#[Validator("en_title", NotBlank::class)]
+#[Validator("hu_desc", NotBlank::class)]
+#[Validator("en_desc", NotBlank::class)]
 #[Modelify(\Application\Database\DefaultConnection::class, 'timeline', true)]
 class Timeline extends _Timeline{
 
