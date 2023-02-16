9be26c0e275dcf1138e08db740bff1b7567d1aa1
MASO09 <tenzlingerkristof@gmail.com>
2023-02-16 18:17:49 +0100

    mod: student registration starter outlook

--- frontend/web/src/pages/registration/StudentRegistration.svelte
+++ frontend/web/src/pages/registration/StudentRegistration.svelte
@@ -13,30 +13,29 @@
     import {locale} from "svelte-i18n";
     import Footer from "src/components/Footer.svelte";
     import {semesters_hu, semesters_en} from "./data/getSemester.ts";
-    import {programs1,programs2} from "./data/getPrograms.ts";
+    import {programs1, programs2} from "./data/getPrograms.ts";
 
 
     let formData = new StudentFormData();
-    let selectedPrograms=[];
-    let selectedPrograms2=[];
-    let selectedMajor="";
-    let selectedSemester="";
+    let selectedPrograms = [];
+    let selectedPrograms2 = [];
+    let selectedMajor = "";
+    let selectedSemester = "";
     let privacy;
     let covid;
 
     function sendData() {
-        formData.major=selectedMajor.name;
-        formData.semester=selectedSemester.name;
+        formData.major = selectedMajor.name;
+        formData.semester = selectedSemester.name;
         formData.programs = selectedPrograms.concat(selectedPrograms2);
         formData.lang = $locale;
         hirundinidae(formData);
     }
 
 </script>
-
 <div class="container is-max-desktop mt-6 cont">
     <div class="tile is-parent">
-        <div class="tile is-child box formcolor">
+        <div class="tile is-child box form-border">
             <p class="title has-text-centered">{$_('menu.registration.registration')}</p>
             <div class="is-divider is-info" data-content={$_('form.common.dividers.personal_info')}></div>
 
@@ -74,17 +73,19 @@
 
             <br>
             <div class="is-divider is-info" data-content={$_('form.common.dividers.programs')}></div>
-            <label class="label has-text-left">{$_('form.common.programs.label.first')}<b class="has-text-danger">*</b>
+            <label class="label has-text-left">{$_('form.common.programs.label.first')}<b
+                    class="has-text-danger">*</b>
             </label>
             {#each programs1 as prog}
-            <input
-                    type="checkbox"
-                    bind:group={selectedPrograms}
-                    value={prog}
-            /> {prog.name}<br>
+                <input
+                        type="checkbox"
+                        bind:group={selectedPrograms}
+                        value={prog}
+                /> {prog.name}<br>
             {/each}
             <br>
-            <label class="label has-text-left">{$_('form.common.programs.label.second')}<b class="has-text-danger">*</b>
+            <label class="label has-text-left">{$_('form.common.programs.label.second')}<b
+                    class="has-text-danger">*</b>
             </label>
             {#each programs2 as prog}
                 <input
@@ -107,13 +108,13 @@
 
             <div class="field mt-4">
                 <Button type="is-info" on:click={sendData}
-                        disabled>{$_('menu.registration.registration')}</Button><p class="has-text-danger">{$_('form.common.registration.disabled')}</p>
+                        disabled>{$_('menu.registration.registration')}</Button>
+                <p class="has-text-danger">{$_('form.common.registration.disabled')}</p>
                 <!--disabled={!privacy | !covid}-->
             </div>
         </div>
     </div>
 </div>
-
 
 
 <ModalCard bind:active={$active} title={$_('modal.title')}>
@@ -124,3 +125,13 @@
         <p></p>
     </div>
 </ModalCard>
+
+<style>
+    .form-border {
+        border: .6em solid #D3AC2B;
+        background: rgba(93, 93, 93, 0.3);
+
+    }
+
+
+</style>
