0ac7feb97591688d530e4ccf760dba739cebbaaa
MASO09 <tenzlingerkristof@gmail.com>
2023-02-20 16:13:03 +0100

    feat: no data and loading response

--- frontend/web/src/pages/hotels/Accommodations.svelte
+++ frontend/web/src/pages/hotels/Accommodations.svelte
@@ -12,65 +12,79 @@
 
 </script>
 <section class="section">
-    <div class="container">
-        {#each $hotels as hotel}
-            {#if hotel.picture !== null}
-                <div class="card">
-                    <div class="columns">
-                        <div class="card-image column is-two-fifths has-ribbon-left">
-                            <figure class="image is-4by3">
-                                <img src={hotel.picture} alt={hotel.name}>
-                                <p class="ribbon is-small">{hotel.location}</p>
-                            </figure>
-                        </div>
-                        <div class="card-content column is-three-fifths has-text-justified">
-                            <p class="title is-4">{hotel.name} {hotel.star}</p>
-                            {#if $locale === 'hu'}
-                                <p class="subtitle is-7">{hotel.hu_type}</p>
-                                <p class="subtitle is-5">{$_('accommodations.label.description')}</p>
-                                <p>{@html hotel.hu_description}</p>
-                            {/if}
-                            {#if $locale === 'en'}
-                                <p class="subtitle is-7">{hotel.en_type}</p>
-                                <p class="subtitle is-5">{$_('accommodations.label.description')}</p>
-                                <p>{@html hotel.en_description}</p>
-                            {/if}
-                            <br>
-                            <div class="columns">
-                                <div class="column is-two-fifths">
-                                    <p class="subtitle is-5">{$_('accommodations.label.services')} </p>
-                                    {#if $locale === 'hu'}
-                                        {#each hotel.hu_services as service}
-                                            {@html service}<br>
-                                        {/each}
-                                    {/if}
-                                    {#if $locale === 'en'}
-                                        {@html hotel.en_services}
-                                    {/if}
-                                </div>
-                                <div class="column is-three-fifths">
-                                    <p class="subtitle is-5">{$_('accommodations.label.moreinformation')} </p>
-                                    <p>{$_('accommodations.label.telephone')} {hotel.telephone}</p>
-                                    <p>{$_('accommodations.label.email')} <a
-                                            href="mailto:{hotel.email}">{hotel.email}</a>
-                                    </p>
-                                    <p>{$_('accommodations.label.place')} {hotel.location}</p>
-                                    <p>{$_('accommodations.label.website')} <a href={hotel.website}
-                                                                               target="_blank">Link</a>
-                                    </p>
-                                    <p>{$_('accommodations.label.maps')} <a href={hotel.maps} target="_blank">Link</a>
-                                    </p>
-                                    <p></p>
-                                    <p></p>
+    {#if !$loading}
+
+        <div class="container">
+            {#each $hotels as hotel}
+                {#if hotel.picture !== null}
+                    <div class="card">
+                        <div class="columns">
+                            <div class="card-image column is-two-fifths has-ribbon-left">
+                                <figure class="image is-4by3">
+                                    <img src={hotel.picture} alt={hotel.name}>
+                                    <p class="ribbon is-small">{hotel.location}</p>
+                                </figure>
+                            </div>
+                            <div class="card-content column is-three-fifths has-text-justified">
+                                <p class="title is-4">{hotel.name} {hotel.star}</p>
+                                {#if $locale === 'hu'}
+                                    <p class="subtitle is-7">{hotel.hu_type}</p>
+                                    <p class="subtitle is-5">{$_('accommodations.label.description')}</p>
+                                    <p>{@html hotel.hu_description}</p>
+                                {/if}
+                                {#if $locale === 'en'}
+                                    <p class="subtitle is-7">{hotel.en_type}</p>
+                                    <p class="subtitle is-5">{$_('accommodations.label.description')}</p>
+                                    <p>{@html hotel.en_description}</p>
+                                {/if}
+                                <br>
+                                <div class="columns">
+                                    <div class="column is-two-fifths">
+                                        <p class="subtitle is-5">{$_('accommodations.label.services')} </p>
+                                        {#if $locale === 'hu'}
+                                            {#each hotel.hu_services as service}
+                                                {@html service}<br>
+                                            {/each}
+                                        {/if}
+                                        {#if $locale === 'en'}
+                                            {@html hotel.en_services}
+                                        {/if}
+                                    </div>
+                                    <div class="column is-three-fifths">
+                                        <p class="subtitle is-5">{$_('accommodations.label.moreinformation')} </p>
+                                        <p>{$_('accommodations.label.telephone')} {hotel.telephone}</p>
+                                        <p>{$_('accommodations.label.email')} <a
+                                                href="mailto:{hotel.email}">{hotel.email}</a>
+                                        </p>
+                                        <p>{$_('accommodations.label.place')} {hotel.location}</p>
+                                        <p>{$_('accommodations.label.website')} <a href={hotel.website}
+                                                                                   target="_blank">Link</a>
+                                        </p>
+                                        <p>{$_('accommodations.label.maps')} <a href={hotel.maps}
+                                                                                target="_blank">Link</a>
+                                        </p>
+                                        <p></p>
+                                        <p></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <br>
-            {/if}
-        {/each}
-    </div>
+                    <br>
+                {/if}
+            {/each}
+
+        </div>
+    {:else if $loading}
+        <div class="has-text-centered is-size-5">
+            <h1>Loading...</h1>
+        </div>
+    {/if}
+    {#if !$loading && $hotels.length === 0}
+        <div class="has-text-centered is-size-5">
+            <h1>{$_('error.nodata')}</h1>
+        </div>
+    {/if}
 </section>
 
 <style>
