676328ed7056b6977a89e3178272f07c4910a799
petinho <petinho92@gmail.com>
2023-02-20 10:36:05 +0100

    mod: updated exist picture conditions

--- src/Missions/Web/Api/HotelApi.php
+++ src/Missions/Web/Api/HotelApi.php
@@ -11,27 +11,29 @@ class HotelApi extends Api
     #[Route(self::GET, '/getcollection/gethotels')]
     public function getHotels()
     {
-       $result = Accommodation::search(Filter::where(Accommodation::active(true)))->collect();
-       $array = array();
-       foreach ($result as $data){
-          $array[] = (object) [
-              'id' => $data->id,
-              'name' => $data->name,
-              'star' => $data->star,
-              'location' => $data->location,
-              'email' => $data->email,
-              'hu_description' => $data->hu_desc,
-              'en_description' => $data->en_desc,
-              'hu_type' => $data->hu_type,
-              'en_type' => $data->en_type,
-              'hu_services' => $data->hu_services,
-              'en_services' => $data->en_services,
-              'telephone' => $data->telephone,
-              'website' => $data->website,
-              'maps' => $data->maps,
-              'picture' => ($data->getAttachmentStorage()->url).($data->picture->first->filename)
-          ];
-       }
-       return $array;
+        $result = Accommodation::search(Filter::where(Accommodation::active(true)))->collect();
+        $array = array();
+        foreach ($result as $data) {
+            if ($data->picture->files) {
+                $array[] = (object)[
+                    'id' => $data->id,
+                    'name' => $data->name,
+                    'star' => $data->star,
+                    'location' => $data->location,
+                    'email' => $data->email,
+                    'hu_description' => $data->hu_desc,
+                    'en_description' => $data->en_desc,
+                    'hu_type' => $data->hu_type,
+                    'en_type' => $data->en_type,
+                    'hu_services' => $data->hu_services,
+                    'en_services' => $data->en_services,
+                    'telephone' => $data->telephone,
+                    'website' => $data->website,
+                    'maps' => $data->maps,
+                    'picture' => ($data->getAttachmentStorage()->url) . ($data->picture->first->filename)
+                ];
+            }
+        }
+        return $array;
     }
 }
