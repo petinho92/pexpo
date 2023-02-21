a49fc9ee466494c0b49e2e27745a52e91c120d8d
MASO09 <tenzlingerkristof@gmail.com>
2023-02-21 17:02:03 +0100

    mod: removed unused import

--- frontend/web/src/pages/welcome/Welcome.svelte
+++ frontend/web/src/pages/welcome/Welcome.svelte
@@ -1,7 +1,6 @@
 <script>
     import Countdown from "./components/Countdown.svelte";
     import Parallax from "./components/Parallax.svelte";
-    import Footer from "./components/Footer.svelte";
     import Timeline3 from "./components/Timeline3.svelte";
     import Sponsors from "./components/Sponsors.svelte";
     import CarouselHero2 from "./components/CarouselHero2.svelte";
