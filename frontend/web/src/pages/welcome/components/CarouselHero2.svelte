b9540cb53a0a89a9911f03af8295250fb54173b3
petinho <petinho92@gmail.com>
2023-02-22 09:38:50 +0100

    modified each block because, getslider endpoint return simple array

--- frontend/web/src/pages/welcome/components/CarouselHero2.svelte
+++ frontend/web/src/pages/welcome/components/CarouselHero2.svelte
@@ -17,14 +17,23 @@
                 dots={false}
                 timingFunction="linear"
         >
-
-            {#each $data[0].imgs as img}
-                <img src={$data[0].url.concat(img)} alt={img}>
+            {#each $data as img}
+                <img class="slider-picture" src={img}>
             {/each}
         </Carousel>
     {/if}
 </section>
 <style>
+    .slider-picture{
+        width: calc(1920px / 2);
+        height: calc(1080px / 2);
+        background-size: cover;
+        position: static;
+        top: 40px;
+        left: 40px;
+        background-position: 34% 46%;
+        transition: height 2s, width 2s;
+    }
     @media screen and (min-width: 769px) {
         img {
             width: calc(1920px / 2);
@@ -32,7 +41,6 @@
             object-fit: cover;
         }
     }
-
     @media screen and (max-width: 768px) {
         img {
             width: calc(1582px / 3);
@@ -40,7 +48,6 @@
             object-fit: cover;
         }
     }
-
     section {
         background-color: #D3AC2B;
     }
