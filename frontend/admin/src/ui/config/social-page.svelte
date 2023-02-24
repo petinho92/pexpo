a499878262134d0d623827f536869a56793249e5
petinho <petinho92@gmail.com>
2023-02-24 15:19:01 +0100

    mod: settings counter

--- frontend/admin/src/ui/config/social-page.svelte
+++ frontend/admin/src/ui/config/social-page.svelte
@@ -1,25 +1,25 @@
 <script type="ts">
-    import counterFormData from "src/ui/config/formData";
-    import FaIcon from "gold-admin/fa-icon";
-    import controls from "gold-admin/form-input/controls";
-    let counter = new counterFormData();
+    import {imask} from '@imask/svelte';
+    import IMask from 'imask'
 
+    let counter = {
+        active: false,
+        counter_date: "",
+        event_date: ""
+    }
+    let okay = false
 
-    let data =[];
+    let data = [];
+
     async function getData() {
-        console.log("getdata")
         const response = await fetch("/api/config/get");
         data = await response.json();
         counter = data.counter;
-        console.log("data:")
-        console.log(data)
-        console.log("counter:")
-        console.log(counter)
-
     }
 
     getData();
-    function handleFormData(){
+
+    function handleFormData() {
         fetch('/api/config/save', {
             method: 'POST',
             headers: {
@@ -28,23 +28,60 @@
             },
             body: JSON.stringify(counter)
         }).then(function (response) {
+            if(response.ok){
+                okay = true;
+            }
         })
+    }
+
+    let momentFormat = 'YYYY-MM-DD HH:mm:ss'
+    const options = {
+        mask: Date,
+        pattern: momentFormat,
+        blocks: {
+            ss: {
+                mask: IMask.MaskedRange,
+                from: 1,
+                to: 59,
+                maxLength: 3,
+            },
+            mm: {
+                mask: IMask.MaskedRange,
+                from: 1,
+                to: 59,
+                maxLength: 2,
+            },
+            HH: {
+                mask: IMask.MaskedRange,
+                from: 1,
+                to: 23,
+                maxLength: 2,
+            },
+            DD: {
+                mask: IMask.MaskedRange,
+                from: 1,
+                to: 31,
+                maxLength: 2,
+            },
+            MM: {
+                mask: IMask.MaskedRange,
+                from: 1,
+                to: 12,
+                maxLength: 2,
+            },
+            YYYY: {
+                mask: IMask.MaskedRange,
+                from: 2022,
+                to: 2050,
+            }
+        },
+
+
     }
 
 </script>
 
 <div class="column p-1 is-full">
-    <div class="card p-0">
-            <header class="card-header  has-background-black">
-                <p class="card-header-title mb-0 is-size-7 p-2 pl-3">
-                        <span class="mr-2"><i class="fas fa-toolbox"></i></span>
-                    Social
-                </p>
-            </header>
-        <div class="content py-2 px-3">
-            Content
-        </div>
-    </div>
     <div class="card p-0">
         <header class="card-header  has-background-black">
             <p class="card-header-title mb-0 is-size-7 p-2 pl-3">
@@ -53,14 +90,21 @@
             </p>
         </header>
         <div class="content py-2 px-3">
-            <label class="label is-small has-text-primary-dark">Aktív:
+            <label class="label is-small has-text-primary-dark">Aktív:</label>
             <label class="switch">
-                 <input type="checkbox" bind:checked={counter.active} />
+                <input type="checkbox" bind:checked={counter.active}/>
                 <span class="slider round"></span>
             </label>
-            </label>
+
             <br>
-            <label class="label is-small has-text-primary-dark">Megnyitó időpontja:</label><input class="input is-small" type="datetime-local" bind:value={counter.date} min="2023-01-01T08:30"/>
+            <!--            <input type="text" pattern="(20[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9])" placeholder="YYYY-MM-DD HH:MM:SS" class="masked" data-valid-example="2023-04-13 09:30:00"/>-->
+            <label class="label is-small has-text-primary-dark">Megnyitó időpontja (YYYY-MM-DD HH:MM:SS)</label><input
+                use:imask={options} bind:value={counter.counter_date}>
+            <label class="label is-small has-text-primary-dark">Esemény időpontja (YYYY.MM.DD-DD.)</label><input
+                bind:value={counter.event_date}>
+            {#if okay}
+                <label class="label is-small has-background-success has-text-white">Sikeresen frissítve</label>
+            {/if}
         </div>
     </div>
     <br>
@@ -71,13 +115,17 @@
   :root {
     --percentage: 0.6;
   }
-  .card {background-color: #000c}
+
+  .card {
+    background-color: #000c
+  }
+
 
   .switch {
     position: relative;
     display: inline-block;
-    width: calc(60px*var(--percentage));
-    height: calc(34px*var(--percentage));
+    width: calc(60px * var(--percentage));
+    height: calc(34px * var(--percentage));
   }
 
   .switch input {
@@ -101,10 +149,10 @@
   .slider:before {
     position: absolute;
     content: "";
-    height: calc(26px*var(--percentage));
-    width: calc(26px*var(--percentage));
-    left: calc(4px*var(--percentage));
-    bottom: calc(4px*var(--percentage));
+    height: calc(26px * var(--percentage));
+    width: calc(26px * var(--percentage));
+    left: calc(4px * var(--percentage));
+    bottom: calc(4px * var(--percentage));
     background-color: white;
     -webkit-transition: .4s;
     transition: .4s;
@@ -119,19 +167,20 @@
   }
 
   input:checked + .slider:before {
-    -webkit-transform: translateX(calc(26px*var(--percentage)));
-    -ms-transform: translateX(calc(26px*var(--percentage)));
-    transform: translateX(calc(26px*var(--percentage)));
+    -webkit-transform: translateX(calc(26px * var(--percentage)));
+    -ms-transform: translateX(calc(26px * var(--percentage)));
+    transform: translateX(calc(26px * var(--percentage)));
   }
 
   /* Rounded sliders */
   .slider.round {
-    border-radius: calc(34px*var(--percentage));
+    border-radius: calc(34px * var(--percentage));
   }
 
   .slider.round:before {
     border-radius: 50%;
   }
+
   input::-webkit-calendar-picker-indicator {
     cursor: pointer;
     padding: 0;
