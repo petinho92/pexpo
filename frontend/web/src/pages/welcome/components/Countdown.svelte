d6e3fa6b25486a14a40317164a5bc08afb99da8c
MASO09 <tenzlingerkristof@gmail.com>
2023-02-24 17:33:27 +0100

    feat: working countdown (if object active do countdown, else "Event ended")

--- frontend/web/src/pages/welcome/components/Countdown.svelte
+++ frontend/web/src/pages/welcome/components/Countdown.svelte
@@ -1,39 +1,56 @@
 <script>
     import Countdown from "svelte-countdown/src/index.js";
     import {_} from 'svelte-i18n';
+    import fetchData from "../../../services/asyncFetch.ts"
+
+    const url = "/config/get";
+    const [data, loading, error, get] = fetchData(url);
 </script>
 
-<section class="section bgcolor">
-    <Countdown from="2023-04-13 09:30:00" dateFormat="YYYY-MM-DD H:m:s" zone="Europe/Budapest" let:remaining>
-        <div class="container">
-            <h1 class="has-text-weight-bold" id="headline">{$_('countdown.text')}</h1>
-<!--            <h1 id="headline">{$_('countdown.end')}</h1>-->
-            <span class="is-size-3">2023.04.13-14.</span>
-            {#if remaining.done === false}
-                <div id="countdown" class="clock">
-                    <ul>
-                        {#if remaining.months > 0}
-                            <li><span class="is-size-4" id="months">{remaining.months}</span><span class="is-size-5">{$_('countdown.month')}</span></li>
-                        {/if}
-                        <li><span class="is-size-4" id="days">{remaining.days}</span><span class="is-size-5">{$_('countdown.day')}</span></li>
-                        <li><span class="is-size-4" id="hours">{remaining.hours} </span><span class="is-size-5">{$_('countdown.hour')}</span></li>
-                        <li><span class="is-size-4" id="minutes">{remaining.minutes}</span><span class="is-size-5">{$_('countdown.minute')}</span></li>
-                        <li><span class="is-size-4" id="seconds">{remaining.seconds}</span><span class="is-size-5">{$_('countdown.second')}</span></li>
-                    </ul>
+{#if !$loading && $data !== null}
+    <section class="section bgcolor">
+        {#if $data.counter.active}
+            <Countdown from="{$data.counter.counter_date}" dateFormat="YYYY-MM-DD H:m:s" zone="Europe/Budapest" let:remaining>
+                <div class="container">
+                    <h1 class="has-text-weight-bold" id="headline">{$_('countdown.text')}</h1>
+                    <span class="is-size-3">{$data.counter.event_date}</span>
+                    {#if remaining.done === false}
+                        <div id="countdown" class="clock">
+                            <ul>
+                                {#if remaining.months > 0}
+                                    <li><span class="is-size-4" id="months">{remaining.months}</span><span
+                                            class="is-size-5">{$_('countdown.month')}</span></li>
+                                {/if}
+                                <li><span class="is-size-4" id="days">{remaining.days}</span><span
+                                        class="is-size-5">{$_('countdown.day')}</span></li>
+                                <li><span class="is-size-4" id="hours">{remaining.hours} </span><span
+                                        class="is-size-5">{$_('countdown.hour')}</span></li>
+                                <li><span class="is-size-4" id="minutes">{remaining.minutes}</span><span
+                                        class="is-size-5">{$_('countdown.minute')}</span></li>
+                                <li><span class="is-size-4" id="seconds">{remaining.seconds}</span><span
+                                        class="is-size-5">{$_('countdown.second')}</span></li>
+                            </ul>
+                        </div>
+                    {:else}
+                        <div id="content" class="emoji">
+                            <span>🥳</span>
+                            <span>🎉</span>
+                            <span>🎂</span>
+                        </div>
+                    {/if}
                 </div>
-            {:else}
-                <div id="content" class="emoji">
-                    <span>🥳</span>
-                    <span>🎉</span>
-                    <span>🎂</span>
-                </div>
-            {/if}
-        </div>
-    </Countdown>
-</section>
+            </Countdown>
+        {:else}
+            <div class="content has-text-centered">
+                <h1>{$_('countdown.end')}</h1>
+            </div>
+
+        {/if}
+    </section>
+{/if}
 <style>
 
-    .clock{
+    .clock {
         box-shadow: 0 15px 25px rgba(129, 124, 124, 0.2);
         border-radius: 5px;
         backdrop-filter: blur(14px);
