482a132c0b3408f81be5980762afb6d0119f4fd4
petinho <petinho92@gmail.com>
2023-02-27 08:13:20 +0100

    add&mod: add discount ribbon to picture and fix en_services each method

--- frontend/web/src/pages/hotels/Accommodations.svelte
+++ frontend/web/src/pages/hotels/Accommodations.svelte
@@ -21,7 +21,12 @@
                             <div class="card-image column is-three-fifths has-ribbon-left">
                                 <figure class="image is-4by3">
                                     <img src={hotel.picture} alt={hotel.name}>
-                                    <p class="ribbon is-small">{hotel.location}</p>
+                                    <!--                                    <p class="ribbon is-small is-danger">{hotel.location}</p>-->
+                                    {#if hotel.discount == 1}
+
+                                        <div class="discount"><span>{$_('accommodations.label.discount')}</span></div>
+
+                                    {/if}
                                 </figure>
                             </div>
                             <div class="card-content column is-one-fifths has-text-justified">
@@ -46,7 +51,9 @@
                                             {/each}
                                         {/if}
                                         {#if $locale === 'en'}
-                                            {@html hotel.en_services}
+                                            {#each hotel.en_services as service}
+                                                {@html service}<br>
+                                            {/each}
                                         {/if}
                                     </div>
                                     <div class="column is-three-fifths">
@@ -87,6 +94,58 @@
 </section>
 
 <style>
+    .discount {
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        z-index: 1;
+        overflow: hidden;
+        width: 95px;
+        height: 95px;
+        text-align: right;
+    }
+
+    .discount span {
+        font-family: 'cairo', serif;
+        font-size: 14px;
+        color: #fff;
+        text-transform: uppercase;
+        text-align: center;
+        line-height: 20px;
+        transform: rotate(-45deg);
+        -webkit-transform: rotate(-45deg);
+        width: 130px;
+        display: block;
+        background: #C8102E;
+        position: absolute;
+        top: 30px;
+        left: -25px;
+    }
+
+    .discount span::before {
+        content: "";
+        position: absolute;
+        left: 0px;
+        top: 100%;
+        z-index: -1;
+        border-left: 3px solid #C8102E;
+        border-right: 3px solid transparent;
+        border-bottom: 3px solid transparent;
+        border-top: 3px solid #C8102E;
+    }
+
+    .discount span::after {
+        content: "";
+        position: absolute;
+        right: 0px;
+        top: 100%;
+        z-index: -1;
+        border-left: 3px solid transparent;
+        border-right: 3px solid #C8102E;
+        border-bottom: 3px solid transparent;
+        border-top: 3px solid #C8102E;
+    }
+
     section {
         min-height: 30em;
         background: #F5F5F5;
