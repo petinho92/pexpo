2fe379178df68dcfe514f6723ff59b3c4154d05b
petinho <petinho92@gmail.com>
2023-03-06 10:08:21 +0100

    mod: extended save method with regustration

--- src/Missions/Admin/Api/ConfigApi.php
+++ src/Missions/Admin/Api/ConfigApi.php
@@ -17,18 +17,22 @@ class ConfigApi extends Api
      * @throws ValidationError
      */
     #[Route(self::POST, "/save")]
-    public function saveConfig(){
+    public function saveConfig()
+    {
         $input = $this->data->all();
         $first = Config::search()->asc("id")->collect();
-        debug($first);
-        $first !==null && $first[0]->id !==null ? $config = Config::pick($first[0]->id) : $config = Config::create();
-        $config->counter = $input;
+        $first !== null && $first[0]->id !== null ? $config = Config::pick($first[0]->id) : $config = Config::create();
+        $config->counter = $input["counter1"];
+        $config->registration = $input['registration1'];
         $config->save();
+
     }
 
     #[Route(self::GET, "/get")]
-    public function getConfig(){
+    public function getConfig()
+    {
         $first = Config::search()->asc("id")->collect();
+        debug($first);
         return Config::pick($first[0]->id);
     }
 }
