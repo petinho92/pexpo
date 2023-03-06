34a7dff21f3bd838adc621bf800e2b558d2c5c3e
petinho <petinho92@gmail.com>
2023-03-06 13:53:12 +0100

    add: RegistrationApi

---
+++ src/Missions/Web/Api/registration/RegistrationApi.php
@@ -0,0 +1,28 @@
+<?php namespace Application\Missions\Web\Api\registration;
+
+use Application\Missions\Web\Services\StudentService;
+use Atomino\Carbon\ValidationError;
+use Atomino\Mercury\Responder\Api\Api;
+use Atomino\Mercury\Responder\Api\Attributes\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegistrationApi extends Api{
+    public function __construct(private StudentService $studentService)
+    {
+    }
+
+    #[Route(self::POST, '/save')]
+    public function studentPost()
+    {
+        try {
+            $student = $this->studentService->create($this->data->all());
+        } catch (ValidationError $error){
+            $this->setStatusCode(422);
+            return $error->getMessages();
+        }
+        return array(
+          'statusCode' => http_response_code(200),
+        );
+    }
+}
