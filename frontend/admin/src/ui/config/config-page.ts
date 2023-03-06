c31a7272e08e1c468951b517b46680d5115a9869
petinho <petinho92@gmail.com>
2023-03-06 10:06:39 +0100

    mod: rename filename

--- frontend/admin/src/ui/config/config-page.ts
+++ frontend/admin/src/ui/config/config-page.ts
@@ -1,13 +1,13 @@
 import FaIcon from "gold-admin/fa-icon";
 import AbstractPage from "gold-admin/app/abstract-page";
 import type {SvelteComponent} from "svelte";
-import SocialComponent from "./social-page.svelte";
+import ConfigComponent from "./config-page.svelte";
 import {writable} from "svelte/store";
 import type {Writable} from "svelte/store";
 
-export default class SocialPage extends AbstractPage {
+export default class ConfigPage extends AbstractPage {
     public closeable: boolean = false;
     get $title(): Writable<string> {return writable('Config');}
     get $icon(): Writable<FaIcon> {return writable(FaIcon.s('toolbox'));}
-    get component(): typeof SvelteComponent { return SocialComponent; }
+    get component(): typeof SvelteComponent { return ConfigComponent; }
 }
