324eb764509d19506011f11a0db2263813fc8bbd
MASO09 <tenzlingerkristof@gmail.com>
2023-03-07 18:58:59 +0100

    mod: majors combined into one array

--- frontend/web/src/pages/registration/data/getMajor.ts
+++ frontend/web/src/pages/registration/data/getMajor.ts
@@ -1,234 +1,182 @@
-export let majors_hu = [
+export let majors= [
     {
         "id": "1",
-        "name": "Építészmérnöki (BSc)"
+        "name_hu": "Építészmérnöki (BSc)",
+        "name_en": "Architectural Engineering (BSc)"
+
     },
     {
         "id": "2",
-        "name": "Építőmérnöki (BSc)"
+        "name_hu": "Építőmérnöki (BSc)",
+        "name_en": "Civil Engineering (BSc)"
+
     },
     {
         "id": "3",
-        "name": "Építőművészet (BA)"
+        "name_hu": "Építőművészet (BA)",
+        "name_en": "Architectural Art (BA)"
+
     },
     {
         "id": "4",
-        "name": "Gépészmérnöki (BSc)"
+        "name_hu": "Gépészmérnöki (BSc)",
+        "name_en": "Mechanical Engineering (BSc)"
+
     },
     {
         "id": "5",
-        "name": "Ipari termék- és formatervező mérnöki (BSc)"
+        "name_hu": "Ipari termék- és formatervező mérnöki (BSc)",
+        "name_en": "Industrial Design Engineering (BSc)"
+
     },
     {
         "id": "6",
-        "name": "Környezetmérnöki (BSc)"
+        "name_hu": "Környezetmérnöki (BSc)",
+        "name_en": "Environmental Engineering (BSc)"
+
     },
     {
         "id": "7",
-        "name": "Mérnökinformatikus (BSc)"
+        "name_hu": "Mérnökinformatikus (BSc)",
+        "name_en": "Computer Science Engineer (BSc)"
+
+
     },
     {
         "id": "8",
-        "name": "Villamosmérnöki (BSc)"
+        "name_hu": "Villamosmérnöki (BSc)",
+        "name_en": "Electrical Engineering (BSc)"
+
     },
     {
         "id": "9",
-        "name": "Belsőépítész tervezőművész (MA)"
+        "name_hu": "Belsőépítész tervezőművész (MA)",
+        "name_en": "Interior and Spatial Design (MA)"
+
     },
     {
         "id": "10",
-        "name": "Egészségügyi mérnök (MSc)"
+        "name_hu": "Egészségügyi mérnök (MSc)",
+        "name_en": "Biomedical Engineer (MSc)"
+
     },
     {
         "id": "11",
-        "name": "Építész (MSc)"
+        "name_hu": "Építész (MSc)",
+        "name_en": "Architect (MSc)"
+
     },
     {
         "id": "12",
-        "name": "Építőművész (MA)"
+        "name_hu": "Építőművész (MA)",
+        "name_en": "Architectural Design (MA)"
+
     },
     {
         "id": "13",
-        "name": "Gépészmérnöki (MSc)"
+        "name_hu": "Gépészmérnöki (MSc)",
+        "name_en": "Mechanical Engineering (MSc)"
+
     },
     {
         "id": "14",
-        "name": "Mérnökinformatikus (MSc)"
+        "name_hu": "Mérnökinformatikus (MSc)",
+        "name_en": "Computer Science Engineer (MSc)"
+
     },
     {
         "id": "15",
-        "name": "Szerkezet-építőmérnöki (MSc)"
+        "name_hu": "Szerkezet-építőmérnöki (MSc)",
+        "name_en": "Structural Engineering (MSc)"
+
     },
     {
         "id": "16",
-        "name": "Településmérnöki (MSc)"
+        "name_hu": "Településmérnöki (MSc)",
+        "name_en": "Urban Systems Engineering (MSc)"
+
     },
     {
         "id": "17",
-        "name": "Breuer Marcell Doktori Iskola"
+        "name_hu": "Breuer Marcell Doktori Iskola",
+        "name_en": "Marcel Breuer Doctoral School"
+
     },
     {
         "id": "18",
-        "name": "Műszaki felsőoktatási szakképzés (FOKSZ)"
+        "name_hu": "Műszaki felsőoktatási szakképzés (FOKSZ)",
+        "name_en": "Engineer Assistant (FOKSZ)"
+
     },
     {
         "id": "19",
-        "name": "Televíziós műsorkészítő (FOKSZ)"
+        "name_hu": "Televíziós műsorkészítő (FOKSZ)",
+        "name_en": "Television Production (FOKSZ)"
+
     },
     {
         "id": "20",
-        "name": "Építészmérnöki osztatlan"
+        "name_hu": "Építészmérnöki osztatlan",
+        "name_en": "Architecture (O)"
+
     },
     {
         "id": "21",
-        "name": "BIM szakmérnök"
+        "name_hu": "BIM szakmérnök",
+        "name_en": "Specialized engineering program in BIM"
+
     },
     {
         "id": "22",
-        "name": "Atomerőművi üzemeltetési szakember"
+        "name_hu": "Atomerőművi üzemeltetési szakember",
+
     },
     {
         "id": "23",
-        "name": "Atomerőművi üzemeltetési szakmérnök"
+        "name_hu": "Atomerőművi üzemeltetési szakmérnök",
+
     },
     {
         "id": "24",
-        "name": "Hulladékgazdálkodási szakmérnök / műszaki szakember"
+        "name_hu": "Hulladékgazdálkodási szakmérnök / műszaki szakember",
+        "name_en": "Waste Management Engineer / Expert"
+
     },
     {
         "id": "25",
-        "name": "Létesítmény-energetikai szakmérnök"
+        "name_hu": "Létesítmény-energetikai szakmérnök",
+        "name_en": "Specialist Engineer in the Field of Building Energetics Engineering"
+
     },
     {
         "id": "26",
-        "name": "Minőségirányítási rendszermenedzser"
+        "name_hu": "Minőségirányítási rendszermenedzser",
+        "name_en": "Quality Assurance System Manager"
+
     },
     {
         "id": "27",
-        "name": "Nukleáris környezetvédelmi szakmérnök / szakember"
+        "name_hu": "Nukleáris környezetvédelmi szakmérnök / szakember",
+        "name_en": "Nuclear Environmental Protection Engineer / Expert"
+
     },
     {
         "id": "28",
-        "name": "Történeti épületdiagnosztikai és rehabilitációs szakmérnök / szakember"
+        "name_hu": "Történeti épületdiagnosztikai és rehabilitációs szakmérnök / szakember"
     },
     {
         "id": "29",
-        "name": "Városüzemeltető szakmérnök"
+        "name_hu": "Városüzemeltető szakmérnök"
     },
     {
         "id": "30",
-        "name": "Vizuális mediátor, iskolai designer"
+        "name_hu": "Vizuális mediátor, iskolai designer"
     },
     {
         "id": "31",
-        "name": "Zaj- és rezgésvédelmi szakmérnök / szakember"
+        "name_hu": "Zaj- és rezgésvédelmi szakmérnök / szakember",
+        "name_en": "Noise and Vibration Control Engineer / Expert"
+
     }
 ];
 
-export let majors_en = [
-    {
-        "id": "1",
-        "name": "Architectural Engineering (BSc)"
-    },
-    {
-        "id": "2",
-        "name": "Civil Engineering (BSc)"
-    },
-    {
-        "id": "3",
-        "name": "Architectural Art (BA)"
-    },
-    {
-        "id": "4",
-        "name": "Mechanical Engineering (BSc)"
-    },
-    {
-        "id": "5",
-        "name": "Industrial Design Engineering (BSc)"
-    },
-    {
-        "id": "6",
-        "name": "Environmental Engineering (BSc)"
-    },
-    {
-        "id": "7",
-        "name": "Computer Science Engineer (BSc)"
-    },
-    {
-        "id": "8",
-        "name": "Electrical Engineering (BSc)"
-    },
-    {
-        "id": "9",
-        "name": "Interior and Spatial Design (MA)"
-    },
-    {
-        "id": "10",
-        "name": "Biomedical Engineer (MSc)"
-    },
-    {
-        "id": "11",
-        "name": "Architect (MSc)"
-    },
-    {
-        "id": "12",
-        "name": "Architectural Design (MA)"
-    },
-    {
-        "id": "13",
-        "name": "Mechanical Engineering (MSc)"
-    },
-    {
-        "id": "14",
-        "name": "Computer Science Engineer (MSc)"
-    },
-    {
-        "id": "15",
-        "name": "Structural Engineering (MSc)"
-    },
-    {
-        "id": "16",
-        "name": "Urban Systems Engineering (MSc)"
-    },
-    {
-        "id": "17",
-        "name": "Marcel Breuer Doctoral School"
-    },
-    {
-        "id": "18",
-        "name": "Engineer Assistant (FOKSZ)"
-    },
-    {
-        "id": "19",
-        "name": "Television Production (FOKSZ)"
-    },
-    {
-        "id": "20",
-        "name": "Architecture (O)"
-    },
-    {
-        "id": "21",
-        "name": "Specialized engineering program in BIM"
-    },
-    {
-        "id": "24",
-        "name": "Waste Management Engineer / Expert"
-    },
-    {
-        "id": "25",
-        "name": "Specialist Engineer in the Field of Building Energetics Engineering"
-    },
-    {
-        "id": "26",
-        "name": "Quality Assurance System Manager"
-    },
-    {
-        "id": "27",
-        "name": "Nuclear Environmental Protection Engineer / Expert"
-    },
-    {
-        "id": "31",
-        "name": "Noise and Vibration Control Engineer / Expert"
-    }
-
-];
